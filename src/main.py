44d92a38c01936199eca33ab86ba9737e7ff8d78
Nuri Jung <jnooree@snu.ac.kr>
2025-06-27 11:38:05 +0900

    stage-2

--- src/main.py
+++ src/main.py
@@ -7,23 +7,33 @@ import pdbline
 
 
 def read_pdb(pdb: Path):
-    coords: list[np.ndarray] = []
+    models: list[np.ndarray] = []
 
     with open(pdb) as f:
+        model: list[np.ndarray] = []
+
         for line in f:
             record = pdbline.record(line)
-            if record == "ATOM" or record == "HETATM":
+            if record == "MODEL":
+                if model:
+                    models.append(np.stack(model))
+                model = []
+            elif record == "ATOM" or record == "HETATM":
                 if pdbline.res_seq(line) % 5 == 0:
-                    coords.append(pdbline.coordinates(line))
+                    model.append(pdbline.coordinates(line))
 
-    return np.stack(coords)
+        if model:
+            models.append(np.stack(model))
+
+    return models
 
 
 def main():
     pdb_file = Path(sys.argv[1])
-    coords = read_pdb(pdb_file)
-    com = np.mean(coords, axis=0)
-    print(f"{com = }")
+    models = read_pdb(pdb_file)
+    for model, coords in enumerate(models):
+        com = np.mean(coords, axis=0)
+        print(f"{model = }: {com = }")
 
 
 if __name__ == "__main__":
